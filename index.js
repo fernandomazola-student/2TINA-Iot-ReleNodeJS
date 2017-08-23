d6ca41987e3ad1500a5ea8ff5cf91f6f98bd8cae
logonrm <logonrm@fiap.com.br>
2017-08-23 11:11:29 -0300

    instalado firmata no arduino e arquivo index e deu require no johnny five e instanciando um objeto board, inicializando o rele na porta 10

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var app = express();
+var five = require("johnny-five");
+
+//Instanciando um objeto board, que é um objeto que representa a placa (É um método instanciado da biblioteca johnnyfive)
+var board = new five.Board();
+var relay = null;
+
+board.on("ready", function(){
+  relay = new five.Relay(10);
+});
 
 app.get('/ligar', function(req, res){
   res.send("Lampada ligada");
+  //Se tiver o relay disponivel chamamos o metodo on
+  if(relay){
+    relay.on();
+  }
 });
 
 app.get('/desligar', function(req, res){
   res.send("Lampada desligada");
+  if(relay){
+    relay.off();
+  }
 });
 
 app.listen(3000, function(){
